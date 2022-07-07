8559151b1afe97593e1dd5ef44201dfa7dd992b4
Tom <99224451+ato-m-a@users.noreply.github.com>
2022-07-07 10:05:45 +0900

    dd

--- pages/terminal.tsx
+++ pages/terminal.tsx
@@ -18,11 +18,26 @@ const Terminal: Page = () => {
   const router = useRouter();
   const nodeRef = useRef(null);
 
+  // detect mobile device
+  const detector = (userAgent: string) => {
+    const mobileRegex = [
+      /Android/i,
+      /iPhone/i,
+      /iPad/i,
+      /iPod/i,
+      /BlackBerry/i,
+      /Windows Phone/i
+    ];
+
+    return mobileRegex.some(mobile => userAgent.match(mobile));
+  };
+
   const dispatch = useAppDispatch();
   const olderConfig = useAppSelector(selectConfig);
 
   // config
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isMobile, setIsMobile] = useState<boolean>(false);
   const [savedPosition, savePosition] = useState({ x: 0, y: 0 });
   const [expand, setExpand] = useState<boolean>(false);
   const [dragDisable, setDragDisable] = useState<boolean>(false);
@@ -31,6 +46,14 @@ const Terminal: Page = () => {
     savePosition({ x: data.x, y: data.y });
     dispatch(updateConfig({ position: { x: data.x, y: data.y } }));
   };
+
+  useEffect(() => {
+    navigator && setIsMobile(detector(navigator.userAgent));
+  }, []);
+
+  useEffect(() => {
+    setPosition({ x: 0, y: 0 });
+  }, [isMobile]);
 
   useEffect(() => {
     setPosition({ ...olderConfig.position });
@@ -48,13 +71,17 @@ const Terminal: Page = () => {
 
   const expandTerminal = (setType: boolean): void => {
     if (setType && !expand) {
+      setDragDisable(true);
       setPosition({ x: 0, y: 0 });
       setExpand(true);
+      setDragDisable(false);
     } else if (!setType && expand) {
+      setDragDisable(true);
       setPosition({ ...savedPosition });
       setExpand(false);
+      setDragDisable(false);
     }
-  }
+  };
 
   // terminal message state
   
@@ -63,19 +90,23 @@ const Terminal: Page = () => {
       <Draggable position={position} onDrag={(e, data) => trackPos(data)} nodeRef={nodeRef} 
       bounds='parent' disabled={dragDisable}>
         <section className={styles.terminal} id="terminal" ref={nodeRef} style={expand && { width: '100%', height: 'calc(100% - 94px)' }}>
-          <div className={styles.terminal__controlbar} onMouseEnter={() => !expand && setDragDisable(false)}>
+          <div className={styles.terminal__controlbar} onMouseEnter={() => !expand && setDragDisable(false)}
+          onTouchStartCapture={() => !expand && setDragDisable(false)}>
             <div className={styles.terminal__btnzone}>
               <Link href={{ pathname: '/' }}>
                 <span className={styles.btn_red} onTouchStart={goHome} />
               </Link>
-              <span className={styles.btn_yellow} onClick={() => expandTerminal(false)} />
-              <span className={styles.btn_green} onClick={() => expandTerminal(true)} />
+              <span className={styles.btn_yellow} 
+              onClick={() => expandTerminal(false)} onTouchStart={() => expandTerminal(false)} />
+              <span className={styles.btn_green} 
+              onClick={() => expandTerminal(true)} onTouchStart={() => expandTerminal(true)} />
             </div>
             <div className={styles.terminal__title}>
               터미널
             </div>
           </div>
-          <div className={styles.terminal__body} onMouseEnter={() => !expand && setDragDisable(true)}>
+          <div className={styles.terminal__body} onMouseEnter={() => !expand && setDragDisable(true)}
+          onTouchStartCapture={() => !expand && setDragDisable(true)}>
             (base) guest@hong ~ %
           </div>
         </section>

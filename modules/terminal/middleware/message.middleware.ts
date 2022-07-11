2aaaf85f7e1fcc86411137c907b9c28fc4014417
Tom <99224451+ato-m-a@users.noreply.github.com>
2022-07-11 22:43:18 +0900

    dd

--- modules/terminal/middleware/message.middleware.ts
+++ modules/terminal/middleware/message.middleware.ts
@@ -1,8 +1,10 @@
 import type { Command } from '../common';
+import { TerminalService } from '../services';
+import { SudoService } from '../services';
 import app from '../controller';
 
 export const MessageMiddleware = async (command: Command): Promise<Command[]> => {
-  const { session, text, running, args, isSecure } = command;
+  const { session, text, running, args, isSecure, sudo } = command;
 
   const primaryCommand = text.split(' ').at(0);
 
@@ -24,22 +26,54 @@ export const MessageMiddleware = async (command: Command): Promise<Command[]> =>
       ]
     }
   
-    // if sudo
-    const response = await execute(command);
+    try {
+      const response = await execute(command);
 
-    return [
-      { session: `(base) ${session}@hong ~ %`, text },
-      ...response
-    ]
+      return [
+        { session: `(base) ${session}@hong ~ %`, text },
+        ...response
+      ]
+    } catch (error) {
+      TerminalService.clearState();
+      SudoService.clearState();
+      if (error.code === 'ERR_NETWORK') {
+        return [
+          { session: `(base) ${session}@hong ~ %`, text },
+          { session: 'server', text: 'connection failed' }
+        ]
+      } else {
+        return [
+          { session: `(base) ${session}@hong ~ %`, text },
+          { session: `${running}:`, text: 'Internal Server Error' }
+        ]
+      }
+    }
   }
   // 실행 중 앱 있으면 바로 연결
   else {
     const execute = app[running];
-    const response = await execute(command);
 
-    return [
-      { session: `${args}:`, text: isSecure ? '' : text },
-      ...response
-    ];
+    try {
+      const response = await execute(command);
+
+      return [
+        { session: `${args}:`, text: isSecure ? '' : text },
+        ...response
+      ];
+    } catch (error) {
+      TerminalService.clearState();
+      SudoService.clearState();
+      if (error.code === 'ERR_NETWORK') {
+        return [
+          { session: `${args}:`, text: isSecure ? '' : text },
+          { session: 'server', text: 'connection failed' }
+        ]
+      } else {
+        return [
+          { session: `${args}:`, text: isSecure ? '' : text },
+          { session: `${running}:`, text: 'Internal Server Error' }
+        ]
+      }
+    }
   }
 }

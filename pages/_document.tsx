ed14354dd2b300101ed5f7362129a038ea4c6d3b
Tom <99224451+ato-m-a@users.noreply.github.com>
2022-07-13 00:00:50 +0900

    dd

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -31,8 +31,10 @@ const Document = () => {
         {/* preload */}
         <link rel="preload" href="/images/icon_system.svg" type="image/svg+xml" as="image" crossOrigin="anonymous" />
         <link rel="preload" href="/images/icon_terminal_cute.svg" type="image/svg+xml" as="image" crossOrigin="anonymous" />
-        <link rel="preload" href="/images/mac_bg_dark.svg" type="image/svg+xml" as="image" crossOrigin="anonymous" />
-        <link rel="preload" href="/images/mac_bg_light.svg" type="image/svg+xml" as="image" crossOrigin="anonymous" />
+        <link rel="preload" href="/images/background.jpeg" type="image/jpeg" as="image" crossOrigin="anonymous" />
+        <link rel="preload" href="/images/background-light.jpeg" type="image/jpeg" as="image" crossOrigin="anonymous" />
+        {/* <link rel="preload" href="/images/mac_bg_dark.svg" type="image/svg+xml" as="image" crossOrigin="anonymous" />
+        <link rel="preload" href="/images/mac_bg_light.svg" type="image/svg+xml" as="image" crossOrigin="anonymous" /> */}
         <link rel="preload" href="/fonts/SDGothicNEO/AppleSDGothicNeoB.ttf" type="font/ttf" as="font" crossOrigin="anonymous" />
         <link rel="preload" href="/fonts/SDGothicNEO/AppleSDGothicNeoL.ttf" type="font/ttf" as="font" crossOrigin="anonymous" />
         <link rel="preload" href="/fonts/SDGothicNEO/AppleSDGothicNeoR.ttf" type="font/ttf" as="font" crossOrigin="anonymous" />

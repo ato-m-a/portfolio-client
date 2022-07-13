87e89e6ba729ea3cb892874e50d3b6714462178b
Tom <99224451+ato-m-a@users.noreply.github.com>
2022-07-13 10:58:42 +0900

    d

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://xn--s55bw1vqg.com'
+}
--- next-sitemap.config.ts
+++
@@ -1,7 +0,0 @@
-const config: import('next-sitemap').IConfig = {
-  siteUrl: process.env.SITE_URL || 'https://xn--s55bw1vqg.com',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
-}
-
-export default config

7cf47c0e008d68923cb74b7ba5e165c4bce74daf
Tom <99224451+ato-m-a@users.noreply.github.com>
2022-07-13 10:51:41 +0900

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,6 @@ const config = {
   siteUrl: process.env.SITE_URL || 'https://xn--s55bw1vqg.com',
   generateRobotsTxt: true, // (optional)
   // ...other options
-}
+};
 
-export default config
+module.exports = config;

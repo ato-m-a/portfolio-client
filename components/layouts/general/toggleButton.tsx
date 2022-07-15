5bc87a2b884d5b82efc98cb802dba1860fafd3c6
Tom <99224451+ato-m-a@users.noreply.github.com>
2022-07-15 10:13:42 +0900

    animated toggle button

--- components/layouts/general/toggleButton.tsx
+++ components/layouts/general/toggleButton.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from 'react';
+import { animated, useTransition } from 'react-spring';
 
 /* styles */
 import styles from '../../../styles/general-layout.module.scss';
@@ -12,12 +13,58 @@ import useTheme from '../../../hooks/theme/useTheme';
 const ToggleButton = (): ReactElement => {
   const [theme, toggleTheme] = useTheme();
 
+  const isDark = theme === 'dark';
+  const transitions = useTransition(isDark, {
+    initial: {
+      transform: 'scale(1) rotate(0deg)',
+      opacity: 1,
+    },
+    from: {
+      transform: 'scale(0) rotate(-180deg)',
+      opacity: 0,
+    },
+    enter: {
+      transform: 'scale(1) rotate(0deg)',
+      opacity: 1,
+    },
+    leave: {
+      transform: 'scale(0) rotate(180deg)',
+      opacity: 0,
+    },
+    reverse: true,
+  });
+
   return (
     <button type="button" className={styles.button_small} onClick={
       () => theme === 'dark' ? toggleTheme('light') : toggleTheme('dark')
     }>
-      {theme === 'dark' && <BsFillMoonFill />}
-      {theme === 'light' && <BsFillSunFill />}
+      {
+        transitions((style, item) => 
+          item ? (
+            <div style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)'
+            }}>
+              <animated.div style={style} className={styles.svg_wrapper}>
+                <BsFillMoonFill />
+              </animated.div>
+            </div>
+          ) : (
+            <div style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)'
+            }}>
+              <animated.div style={style} className={styles.svg_wrapper}>
+                <BsFillSunFill />
+              </animated.div>
+            </div>
+          )
+        )
+      }
     </button>
   )
 };

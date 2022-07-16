dc25fd7f29a831330732dd2093f0f44cd62a681a
Tom <99224451+ato-m-a@users.noreply.github.com>
2022-07-17 00:17:32 +0900

    ddd

--- hooks/theme/useTheme.ts
+++ hooks/theme/useTheme.ts
@@ -1,29 +1,35 @@
 import { useState, useCallback, useLayoutEffect } from 'react';
+import * as cookie from 'cookie';
 
 /* redux */
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { selectTheme, enableLight, enableDark } from '../../store/reducers/theme';
 
 const useTheme = () => {
-  const dispatch = useAppDispatch();
+  // const dispatch = useAppDispatch();
     
   // redux local storage
-  const localTheme = useAppSelector(selectTheme);
+  // const localTheme = useAppSelector(selectTheme);
   const osTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-  const [theme, setTheme] = useState<'dark' | 'light' | 'default'>(
-    localTheme.theme !== 'default' ? localTheme.theme : osTheme
+  // const [theme, setTheme] = useState<'light' | 'dark' | 'default'>(
+  //   localTheme.theme !== 'default' ? localTheme.theme : osTheme
+  // );
+  const [theme, setTheme] = useState<string>(
+    document.cookie 
+      ? cookie.parse(document.cookie).theme && cookie.parse(document.cookie).theme
+      : osTheme
   );
 
   // toggle method
   const toggleTheme = useCallback(() => {
     // light => dark
-    if (theme === 'light') {
-      dispatch(enableDark());
-    }
-    // dark => light
-    else {
-      dispatch(enableLight());
-    }
+    // if (theme === 'light') {
+    //   dispatch(enableDark());
+    // }
+    // // dark => light
+    // else {
+    //   dispatch(enableLight());
+    // }
 
     const contrastTheme = theme === 'dark' ? 'light' : 'dark';
     const themeColor = theme === 'dark' ? '#fff' : '#252525';
@@ -31,9 +37,10 @@ const useTheme = () => {
     document.querySelector('meta[name=theme-color]').setAttribute('content', themeColor);
     document.getElementById('theme_provider').setAttribute('data-theme', contrastTheme);
     document.cookie = `theme=${contrastTheme}; path=/`;
+    localStorage.setItem('theme', contrastTheme);
     setTheme(contrastTheme);
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [theme]);
+  }, [theme, /*dispatch*/]);
 
   return [theme, toggleTheme] as const;
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppContext, AppProps, AppInitialProps } from 'next/app';
 import { NextComponentType } from 'next';
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment, useEffect } from 'react';
 import { Page } from '../types/page';
 import { Provider } from 'react-redux';
 import Head from 'next/head';
@@ -22,31 +22,31 @@ const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({ Compo
   const getLayout = Component.getLayout ?? (page => page);
   const Layout = Component.layout ?? Fragment;
 
-  const [theme, setTheme] = useState<string>(pageProps.theme);
-  const [themeColor, setThemeColor] = useState<string>('#252525');
-  useEffect(() => {
-    if (theme === 'default') {
-      const osTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-      setThemeColor(osTheme === 'dark' ? '#252525' : '#fff');
-    }
-    else {
-      setThemeColor(theme === 'dark' ? '#252525' : '#fff');
-    }
-  }, [theme]);
+  let theme: string = pageProps.theme;
+  // Link 통하여 route 이동 시
+  if (theme === 'default' && typeof document !== 'undefined') {
+    theme = cookie.parse(document.cookie).theme;
+  }
 
-  useEffect(() => {
-    if (pageProps.theme === 'default' && typeof document !== 'undefined') {
-      document.cookie ? setTheme(cookie.parse(document.cookie).theme) : setTheme('default');
-    }
-  }, [pageProps]);
+  const themeColor: '#fff' | '#252525' | null =
+    theme === 'default' ? null : 
+    theme === 'light' ? '#fff' :
+    '#252525';
   
   return (
     <Provider store={store}>
       <Head>
-        <meta name="theme-color" content={themeColor} />
+        {
+          themeColor ? <meta name="theme-color" content={themeColor} />
+          : 
+          <Fragment>
+            <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#252525" />
+            <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
+          </Fragment>
+        }
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
-      <div id="theme_provider" data-theme={theme}>
+      <div id="theme_provider" data-theme={theme ? theme : 'default'}>
         <Layout>
           {getLayout(<Component {...pageProps} />)}
         </Layout>

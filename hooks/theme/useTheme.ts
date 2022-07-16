54f99c2621587900a8ca4c47e6af8c98d803b616
Tom <99224451+ato-m-a@users.noreply.github.com>
2022-07-16 09:05:10 +0900

    back to 07-15

--- hooks/theme/useTheme.ts
+++ hooks/theme/useTheme.ts
@@ -1,4 +1,5 @@
-import { useState, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import * as cookie from 'cookie';
 
 /* redux */
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
@@ -6,11 +7,15 @@ import { selectTheme, enableLight, enableDark } from '../../store/reducers/theme
 
 const useTheme = () => {
   const dispatch = useAppDispatch();
+
+  // cookie
+  const cookieTheme = typeof document !== 'undefined' && document.cookie ?
+    cookie.parse(document.cookie).theme : 'default';
     
   // redux local storage
   const localTheme = useAppSelector(selectTheme);
   const [theme, setTheme] = useState<'dark' | 'light' | 'default'>(localTheme.theme);
-    
+
   // toggle method
   const toggleTheme = useCallback((value: 'dark' | 'light') => {
     // light => dark
@@ -27,9 +32,19 @@ const useTheme = () => {
     document.querySelector('meta[name=theme-color]').setAttribute('content', themeColor);
     document.getElementById('theme_provider').setAttribute('data-theme', value);
     document.cookie = `theme=${value}; path=/`;
-    localStorage.setItem('theme', value);
     setTheme(value);
   }, [dispatch]);
+
+  // at first visit
+  useEffect(() => {
+    if (theme === 'default' || cookieTheme === 'default') {
+      const localTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark' : 'light';
+      toggleTheme(localTheme);
+      setTheme(localTheme);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [theme]);
 
   return [theme, toggleTheme] as const;
 };

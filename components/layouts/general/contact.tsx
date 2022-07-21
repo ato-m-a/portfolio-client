1ae3eb9914951b5bc75e2d526a19bc1228afb9d2
Tom <99224451+ato-m-a@users.noreply.github.com>
2022-07-22 00:33:29 +0900

    add body-scroll-lock
    to prevent scroll when ContactMe component mounted

--- components/layouts/general/contact.tsx
+++ components/layouts/general/contact.tsx
@@ -1,4 +1,5 @@
 import { ReactElement, useState, useEffect, ChangeEvent } from 'react';
+import { enableBodyScroll, disableBodyScroll } from 'body-scroll-lock';
 import Image from 'next/image';
 import axios from 'axios';
 
@@ -126,10 +127,10 @@ const ContactMe = ({ open, close }: Props): ReactElement => {
     } catch (error) {
       setErrorMessage(error.message);
     }
-  }
+  };
 
   useEffect(() => {
-    if (animate && visible && open) document.body.style.overflowY = 'hidden';
+    if (animate && visible && open) disableBodyScroll(document.body);
 
     if (animate && !visible) {
       setVisible(true);
@@ -139,7 +140,7 @@ const ContactMe = ({ open, close }: Props): ReactElement => {
       setAnimate(open);
     } else {
       setTimeout(() => {
-        document.body.style.overflowY = 'auto';
+        enableBodyScroll(document.body);
         setAnimate(open);
       }, 300);
     }
@@ -150,7 +151,7 @@ const ContactMe = ({ open, close }: Props): ReactElement => {
       if (e.key === 'Escape') {
         closeModal();
       }
-    }
+    };
 
     document.addEventListener('keydown', keyDown);
     return () => {
